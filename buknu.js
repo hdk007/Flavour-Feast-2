bcaa19329cfca98e56393edc1346a764279dc31d
hdk007 <125107357+hdk007@users.noreply.github.com>
2023-10-13 22:47:22 +0530

    Add files via upload

---
+++ buknu.js
@@ -0,0 +1,28 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".menu");
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
+let fname = document.getElementById('name');
+let email = document.getElementById('email');
+let number = document.getElementById('phone');
+let quantity = document.getElementById('productCode');
+let address = document.getElementById('productName');
+
+
+
+
+
+function sendEmail() {
+    Email.send({
+        SecureToken : "115fe041-6594-4860-8628-9540b5dd59ec",
+        To : 'flavourfeast007@gmail.com',
+        From : 'hardiksachan70@gmail.com',
+        Subject : "Orders from " + email.value +" "+ "Name: " + fname.value + " " + "number: " + number.value + " " + "Quantity: " + quantity.value + " " + "Address: " + address.value,
+        Body : 'Buknu'
+    }).then(
+      message => alert('The Order was completed!')
+    );
+}
